8b396bd25ac0bf10da4ce71d86250f07098f6f87
hdcola <huangdong@gmail.com>
2020-09-18 10:49:40 -0400

    使用了Sicheng的代码
    Co-Authored-By: Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>

--- cmdproc/rewardscmd.py
+++ cmdproc/rewardscmd.py
@@ -4,42 +4,29 @@
 支持 /rewards 命令
 """
 
-
 import config
 from telegram import Update
 from telegram.ext import Dispatcher,CommandHandler,CallbackContext
 import random
 
-def help():
-    return """
-    这是小怪兽们的作品集合，也是大家平时使用的Bot，欢迎大家使用。
-    到今天，我竟然只有一个技能，一定是出了什么问题，希望有人能快快给我增加能力！
-
-    /help 查看帮助 作者:hdcola
-    """
-
 def rewards(update : Update, context : CallbackContext):
     rewards = [
-        "爽了！直接加90XP。\nIt's cool! Directly to 90XP.",
-        "你太幸运啦！不用加XP！\nYou're so lucky! No need to add XP!",
-        "普普通通，只有40XP。\nOrdinary, only 40 XP.",
-        "让神来决定你的命运吧！\nLet God decide your fate!"
+        "COOL! +90XP!\n爽！+90XP！", 
+        "Good! +40XP!\n很好！+40XP！", 
+        "Sad... Nothing happens.\n好伤心...什么都没发生。", 
+        "Let BOTGOD decide your fate!\n让BOTGOD决定您的命运吧！"
     ]
-    msg = random.choice(rewards)
-    if msg == rewards[3]:
-        xp = random.randrange(40,120)
-        msg = "%s\n\n亲爱的%s,天神奖励你%sXP，离下一级你还有多远？"%(
-            msg,
-            update.effective_user.first_name,
-            xp
-        )
-        msg = "%s\nDear %s, the gods have rewarded you with %sXP, how far are you from the next level?"%(
-            msg,
-            update.effective_user.first_name,
-            xp
-        )
-    msg += "\n\n作者:hdcola"
-    update.message.reply_text(msg)
+    randomPenalty = random.choice(rewards)
+    randomInt = random.randint(50,150)
+    if randomPenalty == rewards[3]:
+        update.message.reply_text("%s\nDear %s, BOTGOD has decided give you %s, keep up the good work!\n亲爱的%s，BOTGOD已决定奖励您%s XP。继续加油！\nCreator/作者: Sichengthebest"%(
+            randomPenalty,
+            update.message.from_user.first_name,
+            randomInt,
+            update.message.from_user.first_name,
+            randomInt))
+    else:
+        update.message.reply_text("%s\nCreator/作者: Sichengthebest"%(randomPenalty))
 
 def add_dispatcher(dp: Dispatcher):
     dp.add_handler(CommandHandler(["rewards"], rewards))
