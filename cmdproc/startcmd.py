249dc51c52ece227d2e2869add81823e8c60fa87
hdcola <huangdong@gmail.com>
2020-09-19 19:40:33 -0400

    更改help到最新的版本

--- cmdproc/startcmd.py
+++ cmdproc/startcmd.py
@@ -13,15 +13,16 @@ def help():
     这是小怪兽们的作品集合，也是大家平时使用的Bot，欢迎大家使用。
     到今天，我竟然只有一个技能，一定是出了什么问题，希望有人能快快给我增加能力！
 
-    /rewards 奖励大转盘 作者:hdcola
-    /weather 查询天气 作者:hdcola
-    /help 查看帮助 作者:hdcola
+    /weather - 查询天气 
+    /rewards - 奖励大转盘 作者:Sichengthebest
+    /penalties - 处罚大转盘 作者:Sichengthebest
+    /help - 查看帮助
 
     开发者用命令：
-    /info 将任意信息的json内容发给你 作者:hdcola
+    /info 将任意信息的json内容发给你
 
     管理员用命令：
-    /admin 管理机器人 作者:hdcola
+    /admin 管理机器人
     """
 
 def start(update : Update, context : CallbackContext):
