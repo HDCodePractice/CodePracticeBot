90422ee4b8baedba533db7c2a374313ed8835bb4
hdcola <huangdong@gmail.com>
2020-10-06 13:59:18 -0400

    增加加入游戏测试

--- tests/cmdproc/test_guesscmd.py
+++ tests/cmdproc/test_guesscmd.py
@@ -1,9 +1,10 @@
 from cmdproc.guesscmd import guess_start
 import pytest
 from cmdproc import guesscmd
-from tests.conftest import make_command_update
+from tests.conftest import make_callback_query_update, make_command_update
 
 def test_start(monkeypatch):
+    # 测试发出/start命令
     def reply_text(*args, **kwargs):
         assert "猜大小 Noah&hdcola" in args[0]
         assert kwargs['reply_markup'] == guesscmd.init_replay_markup(guesscmd.start_buttons)
@@ -11,3 +12,16 @@ def test_start(monkeypatch):
     update = make_command_update("/guess")
     monkeypatch.setattr(update.message, 'reply_text', reply_text)
     guesscmd.guess_start(update,None)
+    # 测试点add按钮
+    def guess_start_add_answer(*args, **kwargs):
+        # print(f"args:{args}\nkwargs:{kwargs}\n\n")
+        assert args[0] == "加入游戏成功！Join the game successfully!"
+
+    def guess_start_add_edit_message_text(*args, **kwargs):
+        # print(f"args:{args}\nkwargs:{kwargs}\n\n")
+        assert "玩家列表:\nfirst_name" in kwargs['text']
+
+    update = make_callback_query_update("","guess_start:add")
+    monkeypatch.setattr(update.callback_query, 'answer', guess_start_add_answer)
+    monkeypatch.setattr(update.callback_query, 'edit_message_text', guess_start_add_edit_message_text)
+    guesscmd.guess_start_callback(update,None)
