f7b89f65dd0f1c16bc94d0d2898635852717af43
hdcola <huangdong@gmail.com>
2021-01-31 11:35:54 -0500

    加入文件名中非法字符检查

--- cmdproc/youtubemusic.py
+++ cmdproc/youtubemusic.py
@@ -3,14 +3,19 @@ from telegram import BotCommand,InputMediaAudio,PhotoSize
 import pafy
 import os
 import config
+import re
 
+def validateTitle(title):
+    rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+    new_title = re.sub(rstr, "", title)  # 替换为下划线
+    return new_title
 
 def youtubemusic(update,context):
     if len(context.args) == 1:
         url = context.args[0]
         video = pafy.new(url)
         bestaudio = video.getbestaudio(preftype="m4a")
-        filepath = f"/tmp/{bestaudio.title}.{bestaudio.extension}"
+        filepath = f"/tmp/{validateTitle(bestaudio.title)}.{bestaudio.extension}"
         music_size = bestaudio.get_filesize()
         if music_size > 1000*1000*10:
             update.message.reply_text("对不起，这个音乐超过了10MB！Sorry, this music is more than 10MB.")
@@ -19,7 +24,7 @@ def youtubemusic(update,context):
         msg = update.message.reply_photo(open(img,'rb'),caption=f"正在下载你的音乐/Downloading your music of {music_size/1000}KB...")
         bestaudio.download(filepath=filepath,quiet=True)
         msg.edit_media(InputMediaAudio(open(filepath,'rb')),timeout=60)
-        os.remove(filepath)
+        # os.remove(filepath)
     else:
         update.message.reply_text("对不起，需要给我一个像样儿的网址/Sorry, but I need a URL after that.比如:\n/ytm ")
 
