8ee583692b09ed9002e1360a606c7ac257cd21b3
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2022-01-22 10:16:41 -0500

    Update rewardscmd

--- cmdproc/rewardscmd.py
+++ cmdproc/rewardscmd.py
@@ -11,22 +11,12 @@ import random
 
 def rewards(update : Update, context : CallbackContext):
     rewards = [
-        "COOL! +90XP!\n爽！+90XP！", 
-        "Good! +40XP!\n很好！+40XP！", 
+        "COOL! +1XP!\n爽！+1XP！", 
+        "Good! +2XP!\n很好！+2XP！", 
+        "POG! +3XP!\n很好！+2XP！", 
         "Sad... Nothing happens.\n好伤心...什么都没发生。", 
-        "Let BOTGOD decide your fate!\n让BOTGOD决定您的命运吧！"
     ]
-    randomPenalty = random.choice(rewards)
-    randomInt = random.randint(50,150)
-    if randomPenalty == rewards[3]:
-        update.message.reply_text("%s\nDear %s, BOTGOD has decided give you %sXP, keep up the good work!\n亲爱的%s，BOTGOD已决定奖励您%s XP。继续加油！\nCreator/作者: Sichengthebest"%(
-            randomPenalty,
-            update.message.from_user.first_name,
-            randomInt,
-            update.message.from_user.first_name,
-            randomInt))
-    else:
-        update.message.reply_text("%s\nCreator/作者: Sichengthebest"%(randomPenalty))
+    update.message.reply_text("%s\nCreator/作者: Sichengthebest, Parker"%(randomPenalty))
 
 def add_dispatcher(dp: Dispatcher):
     dp.add_handler(CommandHandler(["rewards"], rewards))
